69be62bd932df0166db710dda22eeaae9115e91d
Al-Hasib <mdabdullahalhasib1111@gmail.com>
2025-01-20 15:02:17 +0600

    update

--- verification_email.py
+++ verification_email.py
@@ -2,40 +2,44 @@ import imaplib
 import email
 import re
 
-EMAIL = "alhasib.iu.cse@gmail.com"
-PASSWORD = "mnmw uwqz osap rpkv"
-IMAP_SERVER = "imap.gmail.com"
 
-mail = imaplib.IMAP4_SSL(IMAP_SERVER)
-mail.login(EMAIL, PASSWORD)
-mail.select("Inbox")
+def verfication_code():
+    EMAIL = "alhasib.iu.cse@gmail.com"
+    PASSWORD = "mnmw uwqz osap rpkv"
+    IMAP_SERVER = "imap.gmail.com"
+    key = "FROM"
+    from_mail = "mdabdullahalhasib1111@gmail.com"
 
-key = "FROM"
-from_mail = "mdabdullahalhasib1111@gmail.com"
-status, ids = mail.search(None, key, from_mail)
+    mail = imaplib.IMAP4_SSL(IMAP_SERVER)
+    mail.login(EMAIL, PASSWORD)
+    mail.select("Inbox")
 
-# print(status)
-# print(ids)
 
-ids_list = ids[0].split()[-1]
-typ, data = mail.fetch(ids_list, '(RFC822)')
-# print(typ, data)
-# print(ids_list)
-# print(len(data))
-for response in data:
-    if type(response) is tuple:
-        msg = email.message_from_bytes(response[1])
-        print(msg['subject'])
-        print(msg['from'])
+    status, ids = mail.search(None, key, from_mail)
 
-        for part in msg.walk():
-            if part.get_content_type()=="text/plain":
-                msg_body = part.get_payload()
-                print(msg_body)
-# Regex pattern for verification code
-CODE_PATTERN = r'\b\d{6}\b'
-code = re.search(CODE_PATTERN, msg_body).group()
-print(code)
+    # print(status)
+    # print(ids)
+
+    ids_list = ids[0].split()[-1]
+    typ, data = mail.fetch(ids_list, '(RFC822)')
+    # print(typ, data)
+    # print(ids_list)
+    # print(len(data))
+    for response in data:
+        if type(response) is tuple:
+            msg = email.message_from_bytes(response[1])
+            print(msg['subject'])
+            print(msg['from'])
+
+            for part in msg.walk():
+                if part.get_content_type()=="text/plain":
+                    msg_body = part.get_payload()
+                    print(msg_body)
+    # Regex pattern for verification code
+    CODE_PATTERN = r'\b\d{6}\b'
+    code = re.search(CODE_PATTERN, msg_body).group()
+    print(code)
+    return code
 
 
 
